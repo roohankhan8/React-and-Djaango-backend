d101c384e95961510c63e62414be335dd2ee4648
Roohan Khan <roohank8@gmail.com>
2023-08-06 04:08:29 +0500

    .

---
+++ api/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
+# Create your views here.
+
+@api_view(["GET"])
+def getRoutes(request):
+    routes = [
+        {
+            "Endpoint": "/notes/",
+            "method": "GET",
+            "body": None,
+            "description": "Returns an array of notes",
+        },
+        {
+            "Endpoint": "/notes/id",
+            "method": "GET",
+            "body": None,
+            "description": "Returns a single note object",
+        },
+        {
+            "Endpoint": "/notes/create/",
+            "method": "POST",
+            "body": {"body": ""},
+            "description": "Creates new note with data sent in post request",
+        },
+        {
+            "Endpoint": "/notes/id/update/",
+            "method": "PUT",
+            "body": {"body": ""},
+            "description": "Creates an existing note with data sent in post request",
+        },
+        {
+            "Endpoint": "/notes/id/delete/",
+            "method": "DELETE",
+            "body": None,
+            "description": "Deletes and exiting note",
+        },
+    ]
+    return Response(routes)
+
+@api_view(["GET"])
+def getNotes(request):
+    notes=Note.objects.all()
+    serializer=NoteSerializer(notes,many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def getNote(request, pk):
+    note=Note.objects.get(id=pk)
+    serializer=NoteSerializer(note,many=False)
+    return Response(serializer.data)
