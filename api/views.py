bb6bdb372a9cb08006aef532fb89337674740ade
Roohan Khan <roohank8@gmail.com>
2023-08-07 22:06:55 +0500

    .

--- api/views.py
+++ api/views.py
@@ -52,3 +52,27 @@ def getNote(request, pk):
     note=Note.objects.get(id=pk)
     serializer=NoteSerializer(note,many=False)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def createNote(request):
+    data = request.data
+    note = Note.objects.create(body=data['body'])
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def updateNote(request, pk):
+    data = request.data
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(instance=note, data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteNote(request, pk):
+    note=Note.objects.get(id=pk)
+    note.delete()
+    return Response('Note was deleted')
