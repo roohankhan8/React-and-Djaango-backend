ee650e3f17aa97f233cb205f475422f8de66d4a2
Roohan Khan <roohank8@gmail.com>
2023-08-08 03:45:10 +0500

    .

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.getRoutes, name="routes"),
+    # path('', views.getRoutes, name="routes"),
     path('notes/', views.getNotes, name="notes"),
     path('notes/create/', views.createNote, name="create-note"),
     path('notes/<str:pk>/update/', views.updateNote, name="update-note"),
--- api/views.py
+++ api/views.py
@@ -9,41 +9,41 @@ from api import serializers
 from .utils import updateNote, getNoteDetail, deleteNote, getNotesList, createNote
 
 
-@api_view(["GET"])
-def getRoutes(request):
-    routes = [
-        {
-            "Endpoint": "/notes/",
-            "method": "GET",
-            "body": None,
-            "description": "Returns an array of notes",
-        },
-        {
-            "Endpoint": "/notes/id",
-            "method": "GET",
-            "body": None,
-            "description": "Returns a single note object",
-        },
-        {
-            "Endpoint": "/notes/create/",
-            "method": "POST",
-            "body": {"body": ""},
-            "description": "Creates new note with data sent in post request",
-        },
-        {
-            "Endpoint": "/notes/id/update/",
-            "method": "PUT",
-            "body": {"body": ""},
-            "description": "Creates an existing note with data sent in post request",
-        },
-        {
-            "Endpoint": "/notes/id/delete/",
-            "method": "DELETE",
-            "body": None,
-            "description": "Deletes and exiting note",
-        },
-    ]
-    return Response(routes)
+# @api_view(["GET"])
+# def getRoutes(request):
+#     routes = [
+#         {
+#             "Endpoint": "/notes/",
+#             "method": "GET",
+#             "body": None,
+#             "description": "Returns an array of notes",
+#         },
+#         {
+#             "Endpoint": "/notes/id",
+#             "method": "GET",
+#             "body": None,
+#             "description": "Returns a single note object",
+#         },
+#         {
+#             "Endpoint": "/notes/create/",
+#             "method": "POST",
+#             "body": {"body": ""},
+#             "description": "Creates new note with data sent in post request",
+#         },
+#         {
+#             "Endpoint": "/notes/id/update/",
+#             "method": "PUT",
+#             "body": {"body": ""},
+#             "description": "Creates an existing note with data sent in post request",
+#         },
+#         {
+#             "Endpoint": "/notes/id/delete/",
+#             "method": "DELETE",
+#             "body": None,
+#             "description": "Deletes and exiting note",
+#         },
+#     ]
+#     return Response(routes)
 
 
 @api_view(["GET", "POST"])
