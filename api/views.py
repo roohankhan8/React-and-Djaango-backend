182f715663ca84e4fad207de68a315ddfd17180f
Roohan Khan <roohank8@gmail.com>
2023-08-08 02:07:35 +0500

    .

--- api/views.py
+++ api/views.py
@@ -1,9 +1,13 @@
+from django.http import response
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.serializers import Serializer
 from .models import Note
 from .serializers import NoteSerializer
-# Create your views here.
+from api import serializers
+from .utils import updateNote, getNoteDetail, deleteNote, getNotesList, createNote
+
 
 @api_view(["GET"])
 def getRoutes(request):
@@ -41,38 +45,25 @@ def getRoutes(request):
     ]
     return Response(routes)
 
-@api_view(["GET"])
+
+@api_view(["GET", "POST"])
 def getNotes(request):
-    notes=Note.objects.all()
-    serializer=NoteSerializer(notes,many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        return getNotesList(request)
 
-@api_view(["GET"])
+    if request.method == "POST":
+        return createNote(request)
+    return Response('getNotes')
+
+
+@api_view(["GET", "PUT", "DELETE"])
 def getNote(request, pk):
-    note=Note.objects.get(id=pk)
-    serializer=NoteSerializer(note,many=False)
-    return Response(serializer.data)
+    if request.method == "GET":
+        return getNoteDetail(request, pk)
 
-@api_view(['POST'])
-def createNote(request):
-    data = request.data
-    note = Note.objects.create(body=data['body'])
-    serializer = NoteSerializer(note, many=False)
-    return Response(serializer.data)
+    if request.method == "PUT":
+        return updateNote(request, pk)
 
-@api_view(['PUT'])
-def updateNote(request, pk):
-    data = request.data
-    note = Note.objects.get(id=pk)
-    serializer = NoteSerializer(instance=note, data=data)
-
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
-
-@api_view(['DELETE'])
-def deleteNote(request, pk):
-    note=Note.objects.get(id=pk)
-    note.delete()
-    return Response('Note was deleted')
+    if request.method == "DELETE":
+        return deleteNote(request, pk)
+    return Response('getNote')
